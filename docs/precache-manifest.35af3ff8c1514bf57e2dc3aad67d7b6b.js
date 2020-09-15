247b7155ff57f3f6e16c2a8e2e23f1ba5e376b36
Yeim <brielalica@gmail.com>
2020-09-15 18:26:04 -0400

    Docs agregado

---
+++ docs/precache-manifest.35af3ff8c1514bf57e2dc3aad67d7b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b71068c1cf05552adf278f49b5a6558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61fa20cf70cb1978ac1",
+    "url": "/static/css/main.a3f48a92.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61fa20cf70cb1978ac1",
+    "url": "/static/js/main.22b3ef32.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
